ad7d2879077daa52a2c2e407f5a5b5b0805def0e
dorperlst@gmail.com <dorperlst@gmail.com>
2020-09-26 22:16:42 +0300

    update

--- src/routers/shop.js
+++ src/routers/shop.js
@@ -75,60 +75,58 @@ router.post('/shops',auth, upload.array('myFiles', 12), async  function (req, re
 
 })
  
-router.patch('/shops', upload.array('myFiles', 12) , async (req, res) => {
+router.patch('/shops', admin, upload.array('myFiles', 12) , async (req, res) => {
 
-    res.send("rnewimages")
-    res.redirect('/admin');
-    // shop = req.shop
-    // const allowedUpdates = ['description', 'address','lat', 'long']
-    // var newimages = req.files.map(x => x.filename)
+    shop = req.shop
+    const allowedUpdates = ['description', 'address','lat', 'long']
+    var newimages = req.files.map(x => x.filename)
     
-    // var images = JSON.parse( req.body.images)
-    // shop.images = images.concat(newimages)
-    // allowedUpdates.forEach((update) => shop[update] = req.body[update])
+    var images = JSON.parse( req.body.images)
+    shop.images = images.concat(newimages)
+    allowedUpdates.forEach((update) => shop[update] = req.body[update])
  
-    // shop.abouts = JSON.parse(req.body.abouts);
-    // if(req.body.name != shop.name)
-    // {
-    //     var oldName = shop.name;
-    //     var newName = req.body.name;
-    //     try {
-    //         await Cat.updateMany(
-    //             { tree: { $in: [oldName] }},
-    //             { $set: { "tree.$[element]" : newName } },
-    //             { arrayFilters: [ { "element": oldName } ] }
-    //          )
-    //          await Order.updateMany(
-    //             { shop: oldName},
-    //             { $set: { shop : newName } }
+    shop.abouts = JSON.parse(req.body.abouts);
+    if(req.body.name != shop.name)
+    {
+        var oldName = shop.name;
+        var newName = req.body.name;
+        try {
+            await Cat.updateMany(
+                { tree: { $in: [oldName] }},
+                { $set: { "tree.$[element]" : newName } },
+                { arrayFilters: [ { "element": oldName } ] }
+             )
+             await Order.updateMany(
+                { shop: oldName},
+                { $set: { shop : newName } }
                  
-    //          )
-    //      } catch (e) {
-    //         res.send(e.messaage);
-    //      } 
-    //      try {
-    //         await Product.updateMany(
-    //             { tree: { $in: [oldName] }},
-    //             { $set: { "tree.$[element]" : newName,  "shop" : newName } },
-    //             { arrayFilters: [ { "element": oldName } ] }
-    //          )
+             )
+         } catch (e) {
+            res.send(e.messaage);
+         } 
+         try {
+            await Product.updateMany(
+                { tree: { $in: [oldName] }},
+                { $set: { "tree.$[element]" : newName,  "shop" : newName } },
+                { arrayFilters: [ { "element": oldName } ] }
+             )
          
-    //      } catch (e) {
+         } catch (e) {
 
-    //         print(e);
-    //         res.status(400).send(e.messaage)
+            print(e);
+            res.status(400).send(e.messaage)
 
-    //      } 
-    //      shop.name = newName;
-    // }
+         } 
+         shop.name = newName;
+    }
 
-    // try {
-    //      await shop.save()
-    //      res.redirect('/admin');
-    //     res.send(req.shop)
-    // } catch (e) {
-    //     res.status(400).send(e.messaage)
-    // }
+    try {
+         await shop.save()
+         res.redirect('/admin');
+        res.send(req.shop)
+    } catch (e) {
+        res.status(400).send(e.messaage)
+    }
 })
 
 router.delete('/shops/me', auth, async (req, res) => {
