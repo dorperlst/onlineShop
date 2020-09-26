12e675362c89af3a16952a9b6f7a58a97267d1f4
dorperlst@gmail.com <dorperlst@gmail.com>
2020-09-26 18:07:34 +0300

    update

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+require('./db/mongoose')
+
 const express   = require('express');
+const userRouter = require('./routers/user')
+const orderRouter = require('./routers/order')
+const categoryRouter = require('./routers/cat')
+const shopRouter = require('./routers/shop')
+const productRouter = require('./routers/product')
+require('ejs')
+const path = require('path')
+const hbs = require('hbs')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express();
+const session = require('express-session');
+app.use(session({secret: process.env.SESSION_SECRET}));
+
+app.set('view engine', 'ejs')
+app.use(express.static(publicDirectoryPath))
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+app.use(shopRouter)
+app.use(userRouter)
+app.use(productRouter)
+app.use(orderRouter)
+app.use(categoryRouter)
 app.get('/', async (req, res) => {
      
      
 
  
 
-    res.send("loggg")
+    res.render('login', {
+        title: 'login' 
+    })
 })
-
-const port = process.env.PORT || 3500
-
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
-
 module.exports = app
