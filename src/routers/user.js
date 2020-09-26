0b5f513a4373f75a14b2de96c4d2160f1c2646f6
dorperlst@gmail.com <dorperlst@gmail.com>
2020-09-26 20:19:46 +0300

    update

--- src/routers/user.js
+++ src/routers/user.js
@@ -270,7 +270,7 @@ async function redirectSession(req, res, user, href){
     var hour = 360000000
     req.session.cookie.expires = new Date(Date.now() + hour)
     req.session.cookie.maxAge = hour
-    res.send("login")
+    res.send(token)
 
 }
 
