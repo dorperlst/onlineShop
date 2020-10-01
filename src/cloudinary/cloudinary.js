f418c179cd20f0d447dac2ffd83b8a4edd9e05d8
dorperlst@gmail.com <dorperlst@gmail.com>
2020-10-01 06:43:50 +0300

    cloudinary

---
+++ src/cloudinary/cloudinary.js
@@ -0,0 +1,31 @@
+var cloudinary = require('cloudinary').v2;
+const path = require('path');
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key:process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+    });
+
+
+   
+    module.exports = {
+        upload: (image) => {
+            cloudinary.uploader.upload(
+                "public/uploads/"+image , 
+                {public_id: path.parse(image).name}, 
+                function(error, result) { 
+                    console.log(error) 
+                }
+                );
+        },
+      
+        url: (image) => {
+          return cloudinary.url(path.parse(image).name);
+        },
+        destroy: (image) => {
+            cloudinary.uploader.destroy(path.parse( image ).name, function(result) { console.log(result) });
+
+          },
+      
+       };
